eb6e9c58868955e288a7376da276692827ebec72
jewbre <jewbre18@gmail.com>
2014-11-16 12:27:04 +0100

    User/edit i user/info dodani

---
+++ ferbook/user/edit/index.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vilim Stubičan
+ * Date: 3.11.2014.
+ * Time: 21:05
+ */
+include_once "../../constants.php";
+include_once "../../classes/Crypter.php";
+
+// Define response array
+$response = array("data"=>array(), "error" => array());
+
+// Fetch the data
+$username = $_POST['username'];
+
+
+
+// Check the input information
+
+$db = new PDO("mysql:host=".SQL_HOST.";dbname=".SQL_DBNAME.";", SQL_USERNAME, SQL_PASSWORD);
+$checkUser = $db->prepare("SELECT username FROM user WHERE username = ?");
+$checkUser->bindParam(1,$username);
+$checkUser->setFetchMode(PDO::FETCH_OBJ);
+$checkUser->execute();
+
+$result = $checkUser->fetch();
+
+if($result) {}
+    else {
+        $response["error"]=array(
+            "errNum" => 3,
+            "errInfo" => "User with that username does not exist."
+        );
+
+        echo json_encode($response);
+        die();
+    }
+
+if(isset($_POST["name"]) && isset($_POST["lastname"]) && isset($_POST["password"])) {
+    $password = $_POST["password"];
+    $name = $_POST["name"];
+    $lastname = $_POST["lastname"];
+
+    $passwordNotOk = strlen($password) <= 0  || strlen($password)>100;
+    if($passwordNotOk) {
+        $response["error"]=array(
+            "errNum" => 2,
+            "errInfo" => "Invalid data formats."
+        );
+
+        echo json_encode($response);
+        die();
+    }
+
+    $query = $db->prepare("
+        UPDATE user
+        SET pass = ?, name = ?, last_name = ?
+        WHERE username = ?
+        ");
+    $query->bindParam(1,$password);
+    $query->bindParam(2,$name);
+    $query->bindParam(3,$lastname);
+    $query->bindParam(4,$username);
+
+    $query->execute();
+
+} else if(isset($_POST["name"]) || isset($_POST["lastname"])) {
+    $name = $_POST["name"];
+    $lastname = $_POST["lastname"];
+
+    $query = $db->prepare("
+        UPDATE user
+        SET name = ?, last_name = ?
+        WHERE username = ?
+        ");
+    $query->bindParam(1,$name);
+    $query->bindParam(2,$lastname);
+    $query->bindParam(3,$username);
+
+    $query->execute();
+} else if(isset($_POST["password"])){
+    $password = $_POST["password"];
+    $passwordNotOk = strlen($password) <= 0  || strlen($password)>100;
+    if($passwordNotOk) {
+        $response["error"]=array(
+            "errNum" => 2,
+            "errInfo" => "Invalid data formats."
+        );
+
+        echo json_encode($response);
+        die();
+    }
+
+    $query = $db->prepare("
+        UPDATE user
+        SET pass = ?
+        WHERE username = ?
+        ");
+    $query->bindParam(1,$password);
+    $query->bindParam(2,$username);
+
+    $query->execute();
+} else {
+    $response["error"]=array(
+        "errNum" => 11,
+        "errInfo" => "Not a valid request."
+    );
+
+    echo json_encode($response);
+    die();
+}
+
+
+echo json_encode($response);
+
---
+++ ferbook/user/info/index.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vilim Stubičan
+ * Date: 3.11.2014.
+ * Time: 21:05
+ */
+include_once "../../constants.php";
+include_once "../../classes/Crypter.php";
+
+// Define response array
+$response = array("data"=>array(), "error" => array());
+
+// Check if the data is send in any way
+
+if( !isset($_POST['userId'])) {
+    $response["error"]=array(
+        "errNum" => 1,
+        "errInfo" => "Missing variable data."
+    );
+
+    echo json_encode($response);
+    die();
+};
+
+
+// Fetch the data
+$userId = $_POST['userId'];
+
+
+$db = new PDO("mysql:host=".SQL_HOST.";dbname=".SQL_DBNAME.";", SQL_USERNAME, SQL_PASSWORD);
+
+$checkLogin = $db->prepare("SELECT * FROM user WHERE id = ?");
+$checkLogin->bindParam(1, $userId);
+$checkLogin->setFetchMode(PDO::FETCH_OBJ);
+$checkLogin->execute();
+
+$row = $checkLogin->fetch();
+
+if($row) {
+    $output = array();
+    $output["username"] = $row->username;
+    $output["email"] = $row->mail;
+    $output["name"] = $row->name;
+    $output["lastname"] = $row->last_name;
+    $output["picture"] = $row->picture;
+
+    $response["data"] = $output;
+    echo json_encode($response);
+} else {
+    $response["error"]=array(
+        "errNum" => 10,
+        "errInfo" => "No existing user with that id."
+    );
+
+    echo json_encode($response);
+}
