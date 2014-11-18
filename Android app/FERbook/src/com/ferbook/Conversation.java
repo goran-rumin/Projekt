78ba1f56cbe29e83aa481ba52760205333baea6b
dgecek <david.gecek@gmail.com>
2014-11-18 20:51:15 +0100

    dodan conversation klasa

--- Android app/FERbook/src/com/ferbook/Conversation.java
+++ Android app/FERbook/src/com/ferbook/Conversation.java
@@ -16,6 +16,11 @@ public class Conversation extends AsyncTask<Object, Void, String> {
 	private String error_info=null;
 	private String message=null, senderId=null,  timestamp=null;
 	private int flag=-1;
+	int br_poruka=0;
+	List<String> messages = new ArrayList<String>();
+	List<String> senderIds=new ArrayList<String>();
+	List<String> timestamps= new ArrayList<String>();
+	List<Integer> flags=new ArrayList<Integer>();
 	
 	private prenesi sucelje;
 	//Activity kontekst;
@@ -25,8 +30,8 @@ public class Conversation extends AsyncTask<Object, Void, String> {
     protected String doInBackground(Object... arg0) {
     	String userId1=(String) arg0[0];
     	String userId2 = (String) arg0[1];
-    	int br_poruke = (Integer) arg0[2]; //redni broj poruke
-    	sucelje = (prenesi) arg0[3];
+    	//int br_poruke = (Integer) arg0[2]; //redni broj poruke
+    	sucelje = (prenesi) arg0[2];
     	//kontekst = (ConversationActivity) arg0[3];
     	
     	List<NameValuePair> params= new ArrayList<NameValuePair>();
@@ -41,24 +46,19 @@ public class Conversation extends AsyncTask<Object, Void, String> {
     	ServiceHandler sh = new ServiceHandler();    	
     	String jsonStr = sh.makeServiceCall(url,  ServiceHandler.POST, params);
     	
-    	
+    	JSONArray data=new JSONArray();
     	
     	if(jsonStr != null){
     		try{
     			JSONObject jsonObj = new JSONObject(jsonStr);
-    			JSONArray data = jsonObj.getJSONArray("data");
-    			JSONObject poruka = data.getJSONObject(br_poruke);
-    			 
-    			message = poruka.getString("message");
-    			senderId = poruka.getString("senderid");
-    			timestamp = poruka.getString("timestamp");
-    			flag = poruka.getInt("flag");
-    			
-    		}
+    			data = jsonObj.getJSONArray("data");	//exception
+    			data.get(0); //ako je data prazan ide se na catch i parsiranje errora
+    			}	
     		catch(JSONException e){
     			//if the mapping doesn't exist, tj, ako je data prazan pa data ne postoji:
     			e.printStackTrace();
     			try{
+    				error_info="No messages";
     				JSONObject jsonObj = new JSONObject(jsonStr);
     				JSONObject error = jsonObj.getJSONObject("error");
     				error_info=error.getString("errInfo");
@@ -68,9 +68,43 @@ public class Conversation extends AsyncTask<Object, Void, String> {
     				e1.printStackTrace();
     				//System.out.print("greška u parsiranju");
     			}
+    			return null;
+    		}
+    			
+    			
+    		JSONObject poruka=new JSONObject();
+    			br_poruka=0;
+    			
+    			while(true){
+    				try {
+						poruka = data.getJSONObject(br_poruka);
+    			 
+						message = poruka.getString("message");
+						senderId = poruka.getString("senderid");
+						timestamp = poruka.getString("timestamp");
+						flag = poruka.getInt("flag");
+						//ako sve ovo gore uspije i ako nejde u catch:
+						
+						messages.add(message);
+						senderIds.add(senderId);
+						timestamps.add(timestamp);
+						flags.add(flag);						
+						
+    					br_poruka++;
+    					
+					} catch (JSONException e) {
+						e.printStackTrace();
+						break;
+					}
+    				
+    			}
     			
     		}
-    	}
+    		
+    			
+    			
+    		
+    	
     	else error_info="DB does not respond";
     	
     	
@@ -82,7 +116,7 @@ public class Conversation extends AsyncTask<Object, Void, String> {
     }
 
     protected void onPostExecute() {
-        sucelje.prenesi_conversation(message, senderId, timestamp, flag,  error_info);
+        sucelje.prenesi_conversation(messages, senderIds, timestamps, flags, br_poruka,  error_info);
     }
     
     
@@ -90,7 +124,7 @@ public class Conversation extends AsyncTask<Object, Void, String> {
     
     
     public interface prenesi{
-    	void prenesi_conversation(String message, String SenderId, String timestamp, int flag, String error);
+    	void prenesi_conversation(List<String> messages, List<String> SenderIds, List<String> timestamps, List<Integer> flags, int broj_poruka, String error);
     }
 
 
