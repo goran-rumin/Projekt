820cfb2ee4214b9eb804b6809b9a9771b3eeb7c5
dgecek <david.gecek@gmail.com>
2014-11-18 23:30:40 +0100

    dodan inbox i sredjen info

---
+++ Android app/FERbook/src/com/ferbook/Inbox.java
@@ -0,0 +1,6 @@
+package com.ferbook;
+
+public class Inbox {
+
+}//kako ovo izvesti? :/
+//{ "data" : [{"userId": id, "name":name, "lastname":lastname, "lastMessage" : { "message":message, "senderId": sender, "timestamp":timestamp, "flag":flag}}, {second conversation}, {third conversation}, …, {nth conversation}] , "error" : [] }
--- Android app/FERbook/src/com/ferbook/Info.java
+++ Android app/FERbook/src/com/ferbook/Info.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.os.AsyncTask;
 
 public class Info extends AsyncTask<String, Void, Void> {
 	private prenesi_info sucelje;
-	private static String url="http://vdl.hr/user/info";
+	private static String url="http://vdl.hr/ferbook/user/info";
 	private String userId=null, username=null , name=null, lastName=null, email=null, error_info=null;
 	
 
@@ -22,7 +24,41 @@ public class Info extends AsyncTask<String, Void, Void> {
 		
 		ServiceHandler sh = new ServiceHandler();
     	
-    	//String jsonStr =sh.makeServiceCall(url,  ServiceHandler.POST, params);
+    	String jsonStr =sh.makeServiceCall(url,  ServiceHandler.POST, params);
+    	
+    	
+    	if(jsonStr != null){
+    		try{
+    			JSONObject jsonObj = new JSONObject(jsonStr);
+    			JSONObject data = jsonObj.getJSONObject("data");
+    			//JSONArray data = jsonObj.getJSONArray("data");
+    			
+    			userId = data.getString("userId");
+    			username=data.getString("username");
+    			name=data.getString("name");
+    			lastName=data.getString("lastName");
+    			email=data.getString("email");
+    		
+    		
+    		}
+    		catch(JSONException e){
+    			//if the mapping doesn't exist, tj, ako je data prazan pa userid ne postoji:
+    			e.printStackTrace();
+    			try{
+    				JSONObject jsonObj = new JSONObject(jsonStr);
+    				JSONObject error = jsonObj.getJSONObject("error");
+    				error_info=error.getString("errInfo");
+    				
+    			}
+    			catch(JSONException e1){
+    				e1.printStackTrace();
+    				//System.out.print("greška u parsiranju");
+    			}
+    			
+    		}
+    	}
+    	else error_info="DB does not respond";
+    	
 		
 		return null;
 	}
