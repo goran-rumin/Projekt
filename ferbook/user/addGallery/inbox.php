442aa443943e608f8cb82353d4c4f4a43c22c406
Warbringer007 <daniman24@gmail.com>
2014-12-08 17:49:09 +0100

    Dodan servis za dodavanje novog praznog albuma

---
+++ ferbook/user/addGallery/inbox.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Davor
+ * Date: 8.12.2014.
+ * Time: 17:34
+ */
+
+include_once "../../constants.php";
+include_once "../../classes/Crypter.php";
+
+// Define response array
+$response = array("data"=>[], "error" => []);
+
+// Check if the data is send in any way
+if( !isset($_POST['userId']) || !isset($_POST['name'])) {
+    $response["error"]=array(
+        "errNum" => 1,
+        "errInfo" => "Missing variable data."
+    );
+    echo json_encode($response);
+    die();
+};
+// Fetch the data
+
+$userId = $_POST['userId'];
+$name = $_POST['name'];
+$db = new PDO("mysql:host=".SQL_HOST.";dbname=".SQL_DBNAME.";", SQL_USERNAME, SQL_PASSWORD);
+$addAlbum = $db->prepare("INSERT INTO album(name, creator) VALUES (?,?)");
+$addAlbum->bindParam(1, $name);
+$addAlbum->bindParam(2, $userId);
+$addAlbum->setFetchMode(PDO::FETCH_OBJ);
+$addAlbum->execute();
+$addAlbum = $db->prepare("SELECT id FROM album WHERE name = ? AND creator = ?");
+$addAlbum->bindParam(1, $name);
+$addAlbum->bindParam(2, $userId);
+$addAlbum->setFetchMode(PDO::FETCH_OBJ);
+$addAlbum->execute();
+$output = array(
+    "albumId" => $addAlbum->fetch()->id
+);
+$response["data"] = $output;
+echo json_encode($response);
