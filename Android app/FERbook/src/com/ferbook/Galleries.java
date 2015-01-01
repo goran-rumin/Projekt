57bed02eb55cee3a29e4fede5081cf3cc1f2cdbb
dgecek <david.gecek@gmail.com>
2015-01-01 17:57:36 +0100

    dodane galerije i getLikes

---
+++ Android app/FERbook/src/com/ferbook/Galleries.java
@@ -0,0 +1,133 @@
+package com.ferbook;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+
+public class Galleries extends AsyncTask<Object, Void, Void> {
+	
+	private String error_info=null;
+	private String albumId=null, name=null, url_slike=null;
+	
+	private int br_galerija=0;
+	
+		
+	List<String> albumIds = new ArrayList<String>();
+	List<String> names = new ArrayList<String>();
+	List<Drawable> naslovnice = new ArrayList<Drawable>();
+	
+	
+	
+	private prenesi sucelje;
+	//Activity kontekst;
+	private static String url = "http://vdl.hr/ferbook/user/galleries/index.php";
+	
+	
+    protected Void doInBackground(Object... arg0) {    //(userId, sucelje )
+    	//String postId=(String) arg0[0];
+
+    	sucelje = (prenesi) arg0[1];
+    	
+    	List<NameValuePair> params= new ArrayList<NameValuePair>();
+    	
+    	NameValuePair user=new BasicNameValuePair("userId", (String) arg0[0]);    	
+    	params.add(user);            	
+    	
+    	ServiceHandler sh = new ServiceHandler();    	
+    	String jsonStr = sh.makeServiceCall(url,  ServiceHandler.POST, params);
+    	
+    	JSONArray data=new JSONArray();
+    	
+    	if(jsonStr != null){
+    		try{
+    			JSONObject jsonObj = new JSONObject(jsonStr);
+    			data = jsonObj.getJSONArray("data");
+    			data.get(0); //ako je data prazan ide se na catch i parsiranje errora
+    		}catch(JSONException e){
+    			//if the mapping doesn't exist, tj, ako je data prazan pa data ne postoji:
+    			e.printStackTrace();
+    			try{
+    				error_info="No galleries";		//ako je data s razlogom prazan
+    				JSONObject jsonObj = new JSONObject(jsonStr);
+    				JSONObject error = jsonObj.getJSONObject("error");
+    				error_info=error.getString("errInfo");
+    			}catch(JSONException e1){
+    				e1.printStackTrace();
+    				//System.out.print("greška u parsiranju");
+    			}
+    			return null;
+    		}
+    			
+    			
+    		JSONObject gal=new JSONObject();
+    		
+    		br_galerija=0;
+    			
+    			while(true){
+    				try {
+						gal = data.getJSONObject(br_galerija);
+						
+						albumId=gal.getString("albumId");
+						name=gal.getString("name");
+						url_slike=gal.getString("url");
+						
+						//ako sve ovo gore uspije i ako nejde u catch:
+						
+						albumIds.add(albumId);
+						names.add(name);
+						naslovnice.add(vrati_sliku(url_slike));
+											
+									
+						
+    					br_galerija++;
+    					
+					} catch (JSONException e) {
+						break;
+					}
+    				
+    			}	
+    	}
+    	else error_info="DB does not respond";
+    	
+    	
+     return null;
+    }
+
+    protected void onProgressUpdate(Integer... progress) {
+        
+    }
+
+    protected void onPostExecute(Void param) {
+        sucelje.prenesi_getlikes(albumIds, names, naslovnice, br_galerija,error_info);
+    }
+    
+    
+       
+    
+    public interface prenesi{
+    	void prenesi_getlikes(List<String> albumIds, List<String> names, List<Drawable> naslovnice, int broj_galerija, String error);
+    }
+
+    
+    public static Drawable vrati_sliku(String url) {
+	    try {
+	        InputStream is = (InputStream) new URL(url).getContent();
+	        Drawable d = Drawable.createFromStream(is, "src name");
+	        return d;
+	    } catch (Exception e) {
+	        return null;
+	    }
+	}
+
+
+}
---
+++ Android app/FERbook/src/com/ferbook/GetLikes.java
@@ -0,0 +1,147 @@
+package com.ferbook;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+
+public class GetLikes extends AsyncTask<Object, Void, Void> {//{ "data" : [{"likeId":id, "timestamp" : timestamp,"userId":id, "name":name, "lastName":lastname, "picture":url, "username":username, "email": email}, {second comment}, …{nth comment}] , "error" : [] }
+
+	private String error_info=null;
+	private String likeId=null, timestamp=null, userId=null, name=null, lastName=null,  picture=null, username=null, email=null;
+	
+	private int br_likeova=0;
+	
+		
+	List<String> likeIds = new ArrayList<String>();
+	List<String> timestamps = new ArrayList<String>();
+	List<String> userIds = new ArrayList<String>();
+	List<String> names = new ArrayList<String>();
+	List<String> lastNames=new ArrayList<String>();
+	List<Drawable> pictures = new ArrayList<Drawable>();
+	List<String> usernames=new ArrayList<String>();
+	List<String> emails=new ArrayList<String>();
+	
+	
+	private prenesi sucelje;
+	//Activity kontekst;
+	private static String url = "http://vdl.hr/ferbook/post/getlikes/index.php";
+	
+	
+    protected Void doInBackground(Object... arg0) {    //(postId, sucelje )
+    	//String postId=(String) arg0[0];
+
+    	sucelje = (prenesi) arg0[1];
+    	
+    	List<NameValuePair> params= new ArrayList<NameValuePair>();
+    	
+    	NameValuePair user=new BasicNameValuePair("postId", (String) arg0[0]);    	
+    	params.add(user);            	
+    	
+    	ServiceHandler sh = new ServiceHandler();    	
+    	String jsonStr = sh.makeServiceCall(url,  ServiceHandler.POST, params);
+    	
+    	JSONArray data=new JSONArray();
+    	
+    	if(jsonStr != null){
+    		try{
+    			JSONObject jsonObj = new JSONObject(jsonStr);
+    			data = jsonObj.getJSONArray("data");
+    			data.get(0); //ako je data prazan ide se na catch i parsiranje errora
+    		}catch(JSONException e){
+    			//if the mapping doesn't exist, tj, ako je data prazan pa data ne postoji:
+    			e.printStackTrace();
+    			try{
+    				error_info="No likes";		//ako je data s razlogom prazan
+    				JSONObject jsonObj = new JSONObject(jsonStr);
+    				JSONObject error = jsonObj.getJSONObject("error");
+    				error_info=error.getString("errInfo");
+    			}catch(JSONException e1){
+    				e1.printStackTrace();
+    				//System.out.print("greška u parsiranju");
+    			}
+    			return null;
+    		}
+    			
+    			
+    		JSONObject like=new JSONObject();
+    		
+    		br_likeova=0;
+    			
+    			while(true){
+    				try {
+						like = data.getJSONObject(br_likeova);
+						
+						likeId=like.getString("likeId");
+						timestamp=like.getString("timestamp");
+						userId=like.getString("userId");
+						name=like.getString("name");
+						lastName=like.getString("lastName");
+						picture=like.getString("picture");
+						username=like.getString("username");
+						email=like.getString("email");
+						
+						//ako sve ovo gore uspije i ako nejde u catch:
+						
+						likeIds.add(likeId);
+						timestamps.add(timestamp);
+						userIds.add(userId);
+						names.add(name);
+						lastNames.add(lastName);
+						pictures.add(vrati_sliku(picture));
+						usernames.add(username);
+						emails.add(email);
+											
+									
+						
+    					br_likeova++;
+    					
+					} catch (JSONException e) {
+						break;
+					}
+    				
+    			}	
+    	}
+    	else error_info="DB does not respond";
+    	
+    	
+     return null;
+    }
+
+    protected void onProgressUpdate(Integer... progress) {
+        
+    }
+
+    protected void onPostExecute(Void param) {
+        sucelje.prenesi_getlikes(likeIds, timestamps, userIds, names, lastNames, pictures, usernames, emails, br_likeova,error_info);
+    }
+    
+    
+       
+    
+    public interface prenesi{
+    	void prenesi_getlikes(List<String> likeIds, List<String> timestamps, List<String> userIds, List<String> names, List<String> lastNames, List<Drawable> pictures, List<String> usernames, List<String> emails, int broj_likeova, String error);
+    }
+
+    
+    public static Drawable vrati_sliku(String url) {
+	    try {
+	        InputStream is = (InputStream) new URL(url).getContent();
+	        Drawable d = Drawable.createFromStream(is, "src name");
+	        return d;
+	    } catch (Exception e) {
+	        return null;
+	    }
+	}
+
+
+}
