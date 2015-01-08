a24c76453f7d9504e5423bb4eb2b8befbb61ece0
Ante Tomić <Ante.Tomic@fer.hr>
2015-01-08 18:39:49 +0100

    Ispravljeno osvježavanje korisničkih podataka

--- web/script/editinfo.js
+++ web/script/editinfo.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+	$("#bottomDisc").hide();
     $.ajax({
         url: root+"user/active/",
         type : "POST",
@@ -34,17 +34,7 @@ $(document).ready(function() {
 })
 
 function update(userID) {
-    var root= "../../";
-    $.ajax ({
-        url: root +  "user/info/index.php",
-        type : "POST",
-        data : {
-            userId: userID
-        }
-    }).success(function(msg) {
-        var userData = JSON.parse(msg);
-
-    var username = userData.data.username;
+    var username = userID;
     var name = $("#name").val();
     var lastName = $("#lastName").val();
     var password = $("#password").val();
@@ -52,17 +42,22 @@ function update(userID) {
     var root= "../../";
 
     if(password != passwordConfirm) {
-        $("#disclaimer").show();
-        $("#disclaimer").html("Passwords do not match!");
+        $("#bottomDisc").show();
+        $("#bottomDisc").html("Passwords do not match!");
+    }
+	
+	if (name == '' || lastName == '') {
+        $("#bottomDisc").show();
+        $("#bottomDisc").html("Please enter both your first and last names!");
     }
 
     if (name == '' && lastName == '' && password == '') {
-        $("#disclaimer").show();
-        $("#disclaimer").html("Please fill in at least one of the fields!");
+        $("#bottomDisc").show();
+        $("#bottomDisc").html("Please fill in at least one of the fields!");
     }
 
     else if (name != '' && lastName != '' && password != '' && password == passwordConfirm) {
-        $("#disclaimer").hide();
+        $("#bottomDisc").hide();
         $.ajax({
             url: root + "user/edit/index.php",
             type: "POST",
@@ -79,7 +74,7 @@ function update(userID) {
             $("#page").children("input").val("");
         })
     }else if (password == passwordConfirm && password != '' && passwordConfirm != ''){
-        $("#disclaimer").hide();
+        $("#bottomDisc").hide();
         $.ajax({
             url: root + "user/edit/index.php",
             type: "POST",
@@ -94,7 +89,7 @@ function update(userID) {
             $("#page").children("input").val("");
         })
     } else if (name != '' && lastName != ''){
-        $("#disclaimer").hide();
+        $("#bottomDisc").hide();
         $.ajax({
             url: root + "user/edit/index.php",
             type: "POST",
@@ -109,42 +104,16 @@ function update(userID) {
             success();
             $("#page").children("input").val("");
         })
-    } else  if (lastName != ''){
-        $("#disclaimer").hide();
-        $.ajax({
-            url: root + "user/edit/index.php",
-            type: "POST",
-            data: {
-                username: username,
-                lastname: lastName
-            }
-
-        }).success(function (msg) {
-            console.log(msg);
-            success();
-            $("#page").children("input").val("");
-        })
-    } else if (name != '') {
-        $("#disclaimer").hide();
-        $.ajax({
-            url: root + "user/edit/index.php",
-            type: "POST",
-            data: {
-                username: username,
-                name: name
-            }
-
-        }).success(function (msg) {
-            console.log(msg);
-            success();
-            $("#page").children("input").val("");
-        })
-    }
+    } 
 
     function success (){
-        $("#bottomDisc").text("User details changed successfully!");
+        $("#bottomDisc").show();
+        $("#bottomDisc").removeClass("alert-danger").addClass("alert-success");
+        $("#bottomDisc").html("User details changed successfully!");
         setTimeout(function () {
-            $("#bottomDisc").html("")
+            $("#bottomDisc").html("");
+            $("#bottomDisc").removeClass("alert-success").addClass("alert-danger");
+            $("#bottomDisc").hide();
         }, 2000);
     }
     })
